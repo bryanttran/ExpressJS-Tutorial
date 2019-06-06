7afc795f7951afdf219c72a46525ce265d1ff857
Bryant Tran <bryant.t.tran1@gmail.com>
2019-06-06 02:11:59 -0700

    inital commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
+
+
+console.log('test log');
+
+console.log(process.env.MY_SECRET);
